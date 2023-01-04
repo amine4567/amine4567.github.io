05ac2170c82291c518f1e3c0f62d8044301ef52d
amine4567 <souikiamine@gmail.com>
2023-01-04 03:01:23 +0100

    feat: (bendy_bingo) update notes for B1, N1, Y1, E2, D2, Y2, Y3 and B4

--- bendy_bingo/data.js
+++ bendy_bingo/data.js
@@ -12,16 +12,16 @@ const cellsData = {
                 <li><a href="https://retroachievements.org/achievement/233090">Trick or Treat, Smell My Feet (5)</a></li>
                 <li><a href="https://retroachievements.org/achievement/233061">Tankman (5)</a></li>
             </ul>
-            <p>All earned on December 31th, 2022</p>
+            <p>All earned on December 31st, 2022</p>
 
-            <a href="https://retroachievements.org/historyexamine.php?d=1672444800&u=amine456">Link to activity history on December 31th, 2022</a>
+            <a href="https://retroachievements.org/historyexamine.php?d=1672444800&u=amine456">Link to activity history on December 31st, 2022</a>
         `
     },
     "E1": {
         "notes": ""
     },
     "N1": {
-        "notes": ""
+        "notes": '<a href="https://retroachievements.org/achievement/260163">Metal Birds (5)</a>'
     },
     "D1": {
         "notes": ""
@@ -30,32 +30,49 @@ const cellsData = {
         "notes": `
             <a href="https://retroachievements.org/achievement/28312">New Ride (3)</a> <br/>
 
-            <p>Earned on December 31th, 2022</p>
+            <p>Earned on December 31st, 2022</p>
 
-            <a href="https://retroachievements.org/historyexamine.php?d=1672444800&u=amine456">Link to activity history on December 31th, 2022</a>
+            <a href="https://retroachievements.org/historyexamine.php?d=1672444800&u=amine456">Link to activity history on December 31st, 2022</a>
         `
     },
     "B2": {
         "notes": ""
     },
     "E2": {
-        "notes": ""
+        "notes": `
+            <p>Total: 45 points<p>
+            <p>Earned on <a href="https://retroachievements.org/historyexamine.php?d=1672444800&u=amine456">December 31st, 2022 </a></p>
+            <ul>
+                <li><a href="https://retroachievements.org/achievement/106456">Spooktacular (10)</a></li>
+                <li><a href="https://retroachievements.org/achievement/106448">Obelisk Impact (10)</a></li>
+                <li><a href="https://retroachievements.org/achievement/106463">Toon Doom (10)</a></li>
+            </ul>
+            <p>Earned on <a href="https://retroachievements.org/historyexamine.php?d=1672531200&u=amine456">January 1st, 2023</a></p>
+            <ul>
+                <li><a href="https://retroachievements.org/achievement/139941">"Oh No, My Points!" (5)</a></li>
+                <li><a href="https://retroachievements.org/achievement/139943">Chill Pin Perfection (10)</a></li>
+            </ul>
+        `
     },
     "N2": {
         "notes": ""
     },
     "D2": {
         "notes": `
-            <li><a href="https://retroachievements.org/achievement/273141">Muscle! (25)</a>
-            from <a href="https://retroachievements.org/game/21533">~Hack~ Pokemon Regulation Red | Blue [Subset - Solo Challenge] (Game Boy)</a> 
-            Earned on January 1st, 2023</li>
+            <ul>
+                <li><a href="https://retroachievements.org/achievement/276557">[Nuzlocke] Gym Leader Blaine (25)</a></li>
+                <li><a href="https://retroachievements.org/achievement/276558">[Nuzlocke] Gym Leader Giovanni (25)</a></li>
+                <li><a href="https://retroachievements.org/achievement/276559">[Nuzlocke] Hall of Fame (50)</a></li>
+            </ul>
+
+            <p>Earned on <a href="https://retroachievements.org/historyexamine.php?d=1672790400&u=amine456">January 4th, 2023</a></p>
         `
     },
     "Y2": {
         "notes": `
-            <p>Earned 237 points on December 31th, 2022</p>
+            <p>Earned 237 points on December 31st, 2022</p>
 
-            <a href="https://retroachievements.org/historyexamine.php?d=1672444800&u=amine456">Link to activity history on December 31th, 2022</a>
+            <a href="https://retroachievements.org/historyexamine.php?d=1672444800&u=amine456">Link to activity history on December 31st, 2022</a>
         `
     },
     "B3": {
@@ -72,7 +89,7 @@ const cellsData = {
     },
     "Y3": {
         "notes": `
-            <p>All earned from <a href="https://retroachievements.org/game/18282">~Homebrew~ PSXFunkin (PlayStation)</a> on December 31th, 2022</p>
+            <p>All earned from <a href="https://retroachievements.org/game/18282">~Homebrew~ PSXFunkin (PlayStation)</a> on December 31st, 2022</p>
 
             <p>List of achievements:</p>
             <li><a href="https://retroachievements.org/achievement/233056">Spooky Month (5)</a></li>
@@ -83,14 +100,14 @@ const cellsData = {
 
             <br/>
 
-            <a href="https://retroachievements.org/historyexamine.php?d=1672444800&u=amine456">Link to activity history on December 31th, 2022</a>
+            <a href="https://retroachievements.org/historyexamine.php?d=1672444800&u=amine456">Link to activity history on December 31st, 2022</a>
         `
     },
     "B4": {
         "notes": `
-            <p>Earned <a href="https://retroachievements.org/achievement/260354">@EldridgeCrimson, Moyai lol (50)</a> on December 31th, 2022</p>
+            <p>Earned <a href="https://retroachievements.org/achievement/260354">@EldridgeCrimson, Moyai lol (50)</a> on December 31st, 2022</p>
 
-            <a href="https://retroachievements.org/historyexamine.php?d=1672444800&u=amine456">Link to activity history on December 31th, 2022</a>
+            <a href="https://retroachievements.org/historyexamine.php?d=1672444800&u=amine456">Link to activity history on December 31st, 2022</a>
         `
     },
     "E4": {
