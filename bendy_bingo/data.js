ed297c7d1e07bd00caac2dbed1ba4cdc7a596008
amine4567 <souikiamine@gmail.com>
2023-01-08 00:21:09 +0100

    feat: (bendy_bingo) update notes for E1, B2, E2, N2, B3, E3, D3, D4 and B5

--- bendy_bingo/data.js
+++ bendy_bingo/data.js
@@ -18,7 +18,15 @@ const cellsData = {
         `
     },
     "E1": {
-        "notes": ""
+        "notes": `
+            <ol>
+                <li><a href="https://retroachievements.org/achievement/148297">Fashion Icon (1)</a> on 07 Jan, 2023 01:43</li>
+                <li><a href="https://retroachievements.org/achievement/148298"> The Witching Hour (2)</a> on 07 Jan, 2023 01:45</li>
+                <li><a href="https://retroachievements.org/achievement/148299">Outdoor Cat (3)</a> on 07 Jan, 2023 01:46</li>
+            </ol>
+
+            All earned on <a href="https://retroachievements.org/historyexamine.php?d=1673049600&u=amine456">January 7th, 2023</a> 
+        `
     },
     "N1": {
         "notes": '<a href="https://retroachievements.org/achievement/260163">Metal Birds (5)</a>'
@@ -36,26 +44,44 @@ const cellsData = {
         `
     },
     "B2": {
-        "notes": ""
+        "notes": 'Earned 40 points from <a href="https://retroachievements.org/game/12661">Double Dragon (Neo-Geo) (Arcade)</a> on <a href="https://retroachievements.org/historyexamine.php?d=1673049600&u=amine456">January 7th, 2023</a>'
     },
     "E2": {
         "notes": `
-            <p>Total: 45 points<p>
+            <p>Total: 96 points<p>
             <p>Earned on <a href="https://retroachievements.org/historyexamine.php?d=1672444800&u=amine456">December 31st, 2022 </a></p>
             <ul>
                 <li><a href="https://retroachievements.org/achievement/106456">Spooktacular (10)</a></li>
                 <li><a href="https://retroachievements.org/achievement/106448">Obelisk Impact (10)</a></li>
                 <li><a href="https://retroachievements.org/achievement/106463">Toon Doom (10)</a></li>
             </ul>
+
             <p>Earned on <a href="https://retroachievements.org/historyexamine.php?d=1672531200&u=amine456">January 1st, 2023</a></p>
             <ul>
                 <li><a href="https://retroachievements.org/achievement/139941">"Oh No, My Points!" (5)</a></li>
                 <li><a href="https://retroachievements.org/achievement/139943">Chill Pin Perfection (10)</a></li>
             </ul>
+
+            <p>Earned on <a href="https://retroachievements.org/historyexamine.php?d=1673049600&u=amine456">January 7th, 2023</a></p>
+            <ul>
+                <li><a href="https://retroachievements.org/achievement/149625">My Hat is spinning! (2)</a></li>
+                <li><a href="https://retroachievements.org/achievement/149629">Protecting against the Evil (2)</a></li>
+                <li><a href="https://retroachievements.org/achievement/149628">Jumping Higher - Living Better (2)</a></li>
+                <li><a href="https://retroachievements.org/achievement/149870">Is this Portal?! (1)</a></li>
+                <li><a href="https://retroachievements.org/achievement/149626">Shooting Stars (2)</a></li>
+                <li><a href="https://retroachievements.org/achievement/149869">Stopwatch of Time (3)</a></li>
+                <li><a href="https://retroachievements.org/achievement/149630">Monster Slasher (10)</a></li>
+                <li><a href="https://retroachievements.org/achievement/127381">Special Scorer I (3)</a></li>
+                <li><a href="https://retroachievements.org/achievement/127391">Scrap Machine (10)</a></li>
+                <li><a href="https://retroachievements.org/achievement/127365">Worm Mecha (5)</a></li>
+                <li><a href="https://retroachievements.org/achievement/127366">Crablops I (5)</a></li>
+                <li><a href="https://retroachievements.org/achievement/127390">The Future is Now (1)</a></li>
+                <li><a href="https://retroachievements.org/achievement/127367">Crablops II (5)</a></li>
+            </ul>
         `
     },
     "N2": {
-        "notes": ""
+        "notes": 'Mastered on <a href="https://retroachievements.org/historyexamine.php?d=1673049600&u=amine456">January 7th, 2023</a>'
     },
     "D2": {
         "notes": `
@@ -76,16 +102,19 @@ const cellsData = {
         `
     },
     "B3": {
-        "notes": ""
+        "notes": `
+            <p>Earned <a href="https://retroachievements.org/achievement/276545">[Random Pokemon] Gym Leader Erika (5)</a> on <a href="https://retroachievements.org/historyexamine.php?d=1672790400&u=amine456">January 4th, 2023</a></p>
+            <p>276545 / 71 = 3895</p>
+        `
     },
     "E3": {
-        "notes": ""
+        "notes": 'Earned <a href="https://retroachievements.org/achievement/76065">The Alleyway (5)</a> on <a href="https://retroachievements.org/historyexamine.php?d=1673049600&u=amine456">January 7th, 2023</a>'
     },
     "N3": {
         "notes": "FREE POINTS"
     },
     "D3": {
-        "notes": ""
+        "notes": 'Earned <a href="https://retroachievements.org/achievement/59240">Machine Gun Specialization (10)</a> on <a href="https://retroachievements.org/historyexamine.php?d=1673049600&u=amine456">January 7th, 2023</a>'
     },
     "Y3": {
         "notes": `
@@ -117,13 +146,13 @@ const cellsData = {
         "notes": ""
     },
     "D4": {
-        "notes": ""
+        "notes": 'Earned <a href="https://retroachievements.org/achievement/148302">Fast Feline (5)</a> on <a href="https://retroachievements.org/historyexamine.php?d=1673049600&u=amine456">January 7th, 2023</a>'
     },
     "Y4": {
         "notes": ""
     },
     "B5": {
-        "notes": ""
+        "notes": 'Earned on <a href="https://retroachievements.org/historyexamine.php?d=1673049600&u=amine456">January 7th, 2023</a>'
     },
     "E5": {
         "notes": ""
